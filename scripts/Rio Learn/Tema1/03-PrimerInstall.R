e67f5238fe2cb8b23d45f3b323e1e3cee2fb56e7
RioSnake21 <rodrigo.pardo@outlook.com>
2020-05-21 22:47:29 -0400

    Cómo instalar paquetes y cargar librerias en R. Cada vez que inicio una sesión, debo cargar las liberias para poder usarlas

---
+++ scripts/Rio Learn/Tema1/03-PrimerInstall.R
@@ -0,0 +1,20 @@
+#Ejemplo de cómo instalar un paquetes
+
+#Instalar paquete tiduverse:
+
+install.packages("tidyverse", dep = TRUE)
+
+#Pero si veo en la pestaña de paquetes, no está cargado una vez instalado.
+#Para que aparezca instalado, se usa esta línea de código:
+
+library(tidyverse)
+
+#Instalar paquete magic:
+
+install.packages("magic", dep =TRUE)
+
+#Para cargar librerias no se ponen comillas
+
+library(magic)
+
+#Si cierro rstudio (cerrar sesión), debo volver a cargar todas las librerias otra vez.
